3706e0850dd2b7dec7e53455546c147d9e3f41a8
Mikołaj Stępniewski <maikel@MacBook-Pro-Mikoaj.local>
2016-12-13 21:52:48 +0100

    Initial Commit

---
+++ Template/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Template
+//
+//  Created by Mikołaj Stępniewski on 13.12.2016.
+//  Copyright © 2016 Mikołaj Stępniewski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
